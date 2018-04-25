05b7e4479c398abb5219383f01e0ae47a38aa012
Jason Morris <jasonmorris@gmail.com>
2018-04-24 22:46:55 -0400

    Add quicken

--- config/cask.sh
+++ config/cask.sh
@@ -70,6 +70,7 @@ if [ "$1" == "--full" ] || [ "$1" == "-f" ]; then
   brew cask install --appdir="/Applications" microsoft-office
   brew cask install --appdir="/Applications" moom
   brew cask install --appdir="/Applications" namechanger
+  brew cask install --appdir="/Applications" quicken
   brew cask install --appdir="/Applications" scrutiny
   brew cask install --appdir="/Applications" sequel-pro
   brew cask install --appdir="/Applications" sketch
