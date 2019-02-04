040bdd409c778cb08551cbffdb49b9fd36fa031f
Jason Morris <jasonmorris@gmail.com>
2019-02-03 20:17:20 -0500

    update path to list to run from project root

--- config/atom.sh
+++ config/atom.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo -e "\\n⬇️  Install Atom packages and themes"
-apm install --packages-file "./atom.list"
+apm install --packages-file "./config/atom.list"
 
 if [ -f "$HOME/.atom/config.cson" ]; then
   echo -e "\\n🔁  Backup Atom config, copy tracked version"
