fd0aac11ab715000c736058f93646b5afb37664e
Jason Morris <jasonmorris@gmail.com>
2019-11-04 15:49:05 -0500

    Add sublime merge

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -97,6 +97,7 @@ brew cask install --appdir="/Applications" sequel-pro
 brew cask install --appdir="/Applications" sketchup
 brew cask install --appdir="/Applications" slack
 brew cask install --appdir="/Applications" spotify
+brew cask install --appdir="/Applications" sublime-merge
 brew cask install --appdir="/Applications" sublime-text
 brew cask install --appdir="/Applications" superduper
 brew cask install --appdir="/Applications" taskpaper
