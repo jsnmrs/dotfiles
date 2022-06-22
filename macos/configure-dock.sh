7098c3b29150497ced2e390abbd95b9917e4cbcb
Jason Morris <jasonmorris@fastmail.com>
2022-06-22 08:22:22 -0400

    Update dock

--- macos/configure-dock.sh
+++ macos/configure-dock.sh
@@ -110,7 +110,6 @@ add_app_to_dock "Firefox"
 add_spacer_to_dock
 add_app_to_dock "Fantastical"
 add_app_to_dock "OmniFocus"
-add_app_to_dock "TickTick"
 add_app_to_dock "Notes"
 add_app_to_dock "Obsidian"
 add_spacer_to_dock
