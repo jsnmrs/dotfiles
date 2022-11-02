69d775e6406fbe00e050c571710729a8af7a2b7d
Jason Morris <jasonmorris@fastmail.com>
2022-11-01 20:20:59 -0400

    Hold sudo throughout

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 echo -e "\\n⬇️  Set up Homebrew and install almost everything"
 
 if test ! "$(brew --version)"
