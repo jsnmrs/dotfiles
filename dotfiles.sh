3c47f55f4a79ccd661efe371f703cb473b722131
Jason Morris <code@jasonmorris.com>
2025-02-23 16:03:17 -0500

    Move dotfile script to root

--- dotfiles.sh
+++ dotfiles.sh
@@ -8,7 +8,7 @@ echo -e "\\n⬇️  Transfer dotfiles to ~/"
 current_path="$PWD"
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" || exit; pwd -P )
 
-if ! rsync --exclude "install.sh" -avh --no-perms "$parent_path/" "$HOME"; then
+if ! rsync -avh --no-perms "$parent_path/home/" "$HOME"; then
     echo "Error: Failed to sync dotfiles" >&2
     exit 1
 fi
